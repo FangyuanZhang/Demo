93aba139be92f7a6bdf0281e04d1f9ed2102041f
zfy <zhangfangyuan@163.com>
2016-12-02 17:06:21 +0800

    添加两个 新类

---
+++ Student.java
@@ -0,0 +1,42 @@
+class Student {
+
+	private String name;
+	private int age;
+	private float sum;
+	private int pwd;
+	private String enen;
+	private double d;
+
+	void fight(){
+		syso
+		
+		System.out.println("打啥架");
+	}
+
+	
+	void godie(){
+		System.out.println(样,不服你打我啊");
+	}
+
+	void run(){
+
+		System.out.println("洋洋得意");
+
+	
+	}
+	void haha(){
+		syso("haha");
+	}
+	void run(int a){
+
+		System.out.println("洋洋得意"+a);
+
+	
+	}
+
+
+	void eat(){
+	syso
+		}
+	
+}
---
+++ sb.java
@@ -0,0 +1,5 @@
+class SB {
+	void Sb(){
+		System.out.print("ÄãÊÇ²»ÊÇÉµ...");
+	}
+}
